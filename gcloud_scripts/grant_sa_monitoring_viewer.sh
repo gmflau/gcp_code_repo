c3da2bad6be8cea99024b341ecc0642cb48ec69d
gmflau <gmflau@gmail.com>
2023-12-07 10:04:22 -0800

    Added gcloud scripts

---
+++ gcloud_scripts/grant_sa_monitoring_viewer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script grants Monitoring Viewer role to the input(service_account) to Google Cloud projects from '$(gcloud projects list) command'
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 service_account"
+    exit 1
+fi
+
+projects=$(gcloud projects list | awk 'NR>1 {print $1}')
+
+echo "$projects" | while read -r project_id; do
+    project_number=$(gcloud projects describe ${project_id} --format="value(projectNumber)")
+    gcloud projects add-iam-policy-binding ${project_number} --member=serviceAccount:$1 --role='roles/monitoring.viewer' --condition=None
+done
